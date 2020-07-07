01c6287cb5a65ed3a90d33f5cd081c6149574f14
Admin <you@example.com>
2020-07-07 18:20:34 +0300

    added form

--- src/components/person.js
+++ src/components/person.js
@@ -1,10 +1,91 @@
 import React from 'react';
+import { AGES, GENDERS, EDUCATIONS, ENGLISH_LEVELS, POSITIONS } from '../constants';
+
+const renderSelectOptions = (data) => data.map(({value, text}) => (<option key={value} value={value}>{text}</option>));
 
 class Person extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            age: "18-29",
+            gender: "male",
+            experience: 2,
+            education: "hight",
+            englishLevel: "advanced",
+            position: "web-developer",
+            salary: 100
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
+    handleInputChange(event) {
+        const {type, name, value} = event.target;
+
+        this.setState({
+            [name]: type === "number" ? parseInt(value) : value
+        })
+    }
 
     render() {
+        const {name, age, gender, experience, education, englishLevel, position, salary} = this.state;
+
         return (
-            <div>Person page</div>
+            <div>
+                <h2>Person page</h2>
+
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <div>
+                        <label>Name</label>
+                        <input type="text" name="name" value={name} onChange={this.handleInputChange.bind(this)} />
+                    </div>
+                    <div>
+                        <label>Age</label>
+                        <select name="age" value={age} onChange={this.handleInputChange.bind(this)}>
+                            {renderSelectOptions(AGES)}
+                        </select>
+                    </div>
+                    <div>
+                        <label>Gender</label>
+                        <select name="gender" value={gender} onChange={this.handleInputChange.bind(this)}>
+                            {renderSelectOptions(GENDERS)}
+                        </select>
+                    </div>
+                    <div>
+                        <label>Experience</label>
+                        <input type="number" name="experience" value={experience} onChange={this.handleInputChange.bind(this)} />
+                    </div>
+                    <div>
+                        <label>Education</label>
+                        <select name="education" value={education} onChange={this.handleInputChange.bind(this)}>
+                            {renderSelectOptions(EDUCATIONS)}
+                        </select>
+                    </div>
+                    <div>
+                        <label>English level</label>
+                        <select name="englishLevel" value={englishLevel} onChange={this.handleInputChange.bind(this)}>
+                            {renderSelectOptions(ENGLISH_LEVELS)}
+                        </select>
+                    </div>
+                    <div>
+                        <label>Position</label>
+                        <select name="position" value={position} onChange={this.handleInputChange.bind(this)}>
+                            {renderSelectOptions(POSITIONS)}
+                        </select>
+                    </div>
+                    <div>
+                        <label>Salary</label>
+                        <input type="number" name="salary" value={salary} onChange={this.handleInputChange.bind(this)} />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+            
         )
     }
 }
---
+++ src/constants/index.js
@@ -0,0 +1,90 @@
+export const AGES = [
+    {
+        value: "18-29", 
+        text: "18 - 29"
+    },
+    {
+        value: "30-39", 
+        text: "30 - 39"
+    }
+]
+
+export const GENDERS = [
+    {
+        value: "male", 
+        text: "Male"
+    },
+    {
+        value: "female", 
+        text: "Female"
+    }
+]
+
+export const EDUCATIONS = [
+    {
+        value: "hight", 
+        text: "Hight"
+    },
+    {
+        value: "secondary", 
+        text: "Secondary"
+    },
+    {
+        value: "none", 
+        text: "None"
+    }
+]
+
+export const ENGLISH_LEVELS = [
+    {
+        value: "no-knowledge", 
+        text: "No knowledge"
+    },
+    {
+        value: "elementary", 
+        text: "Elementary "
+    },
+    {
+        value: "low-intermediat", 
+        text: "Low intermediat"
+    },
+    {
+        value: "high-intermediate", 
+        text: "High intermediate"
+    },
+    {
+        value: "advanced", 
+        text: "Advanced"
+    },
+    {
+        value: "proficient", 
+        text: "Proficient"
+    }
+]
+
+export const POSITIONS = [
+    {
+        value: "computer-programmer", 
+        text: "Computer programmer"
+    },
+    {
+        value: "web-developer", 
+        text: "Web developer "
+    },
+    {
+        value: "programmer-analyst", 
+        text: "Programmer analyst"
+    },
+    {
+        value: "computer-systems-engineer", 
+        text: "Computer systems engineer"
+    },
+    {
+        value: "software-engineer", 
+        text: "Software engineer"
+    },
+    {
+        value: "mobile-app-developer", 
+        text: "Mobile app developer"
+    }
+]
